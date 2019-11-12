3cc81af4376df6ad07f4d64ee38df00e6aec7891
aselnigmatullina <nogmatullina.asel2016@yandex.ru>
2019-11-12 15:51:59 +0300

    added

---
+++ my-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => 
+    (
+        <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-4">Приложение, написанное на React</h1>
+          <p class="lead">Работу выполнила Нигматуллина А.Р</p>
+        </div>
+      </div>
+      
+    )
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react'
+import {Form} from '../components/form'
+import {Notes} from '../components/Notes'
+
+export const Home = () => {
+    const notes = new Array(3)
+    .fill('')
+    .map(( _,i) => ({ id: 1, title: `Note ${i+1}`}))
+    return(
+        <Fragment>
+            <Form />
+            <hr />
+            <Notes notes = {notes}/>
+        </Fragment>
+    )
+}
